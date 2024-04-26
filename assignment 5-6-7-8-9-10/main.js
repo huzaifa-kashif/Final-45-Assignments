dee32364f59d3022a10c757eae0eec12fb3d9122
huzaifa-kashif <huzaifakashif147@gmail.com>
2024-04-27 03:31:06 +0500

    first commit

---
+++ assignment 5-6-7-8-9-10/main.js
@@ -0,0 +1,37 @@
+"use strict";
+//assignment 05
+let famousPerson = "Franklin D. Roosevelt";
+let message = `${famousPerson} once said "The only thing we have to fear is fear itself" `;
+console.log(message);
+//assignment 06
+let nameWithWhiteSpace = "\t \n huzaifa kashif \t \n";
+console.log("name With White Space:", nameWithWhiteSpace);
+let strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+//assignment 07 
+let additionResult = 5 + 3;
+console.log("Addition Result:", additionResult);
+let subtractionResult = 10 - 2;
+console.log("Subtraction Result:", subtractionResult);
+let multiplicationResult = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult);
+let divisionResult = 16 / 2;
+console.log("Division Result:", divisionResult);
+//assignment 08 
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//assignment 09 
+let favoritedResult = 8;
+let messageResult = `My favorite number: ${favoritedResult}`;
+console.log(messageResult);
+//assignment 10
+//this program adds two number and prints the result
+//define Two numbers 
+let number1 = 10;
+let number2 = 20;
+//add two numbers
+let result = number1 + number2;
+//print Result
+console.log(result);
---
+++ assignment 5-6-7-8-9-10/main.ts
@@ -0,0 +1,54 @@
+//assignment 05
+
+let famousPerson:string = "Franklin D. Roosevelt";
+let message:string = `${famousPerson} once said "The only thing we have to fear is fear itself" ` ;
+console.log(message);
+
+//assignment 06
+
+let nameWithWhiteSpace:string = "\t \n huzaifa kashif \t \n";
+console.log("name With White Space:", nameWithWhiteSpace);
+
+let strippedName:string = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+
+//assignment 07 
+
+let additionResult:number = 5+3;
+console.log("Addition Result:", additionResult);
+
+let subtractionResult:number = 10-2;
+console.log("Subtraction Result:", subtractionResult);
+
+let multiplicationResult:number = 4*2;
+console.log("Multiplication Result:", multiplicationResult);
+
+let divisionResult:number = 16/2;
+console.log("Division Result:", divisionResult);
+
+//assignment 08 
+
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+//assignment 09 
+
+let favoritedResult:number = 8;
+let messageResult:string = `My favorite number: ${favoritedResult}`;
+console.log(messageResult);
+
+//assignment 10
+
+//this program adds two number and prints the result
+
+//define Two numbers 
+let number1:number = 10;
+let number2:number = 20;
+//add two numbers
+let result:number = number1  + number2;
+//print Result
+console.log(result);
+
+
